96615dc1af6e3207eb10206a9e8fb6147e84c6ba
Mikhail <mpolet@outlook.com>
2023-04-26 19:30:09 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,64 @@
+package ru.netology.web.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPageV3;
+
+import javax.xml.crypto.Data;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MoneyTransferTest {
+
+  @Test
+  void shouldTransferMoneyBetweenOwnCardsV3() {
+    var loginPage = open("http://localhost:9999", LoginPageV3.class);
+    var authInfo = DataHelper.getAuthInfo();
+    var verificationPage = loginPage.validLogin(authInfo);
+    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+    var dashboardPage = verificationPage.validVerify(verificationCode);
+    var firstCardInfo = DataHelper.getFirstCardBalance();
+    var secondCardInfo = DataHelper.getSecondCardBalance();
+    var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+    var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+    var amount = DataHelper.generateValidAmount(firstCardBalance);
+    var expectedBalanceFirstCard = firstCardBalance - amount;
+    var expectedBalanceSecondCard = secondCardBalance + amount;
+    var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+    dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+    var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+    var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+    assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+    assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+
+  }
+
+  @Test
+  void shouldTransferMoneyBetweenOwnCardsWhenTransferIsMoreThanBalance() {
+    Configuration.holdBrowserOpen = true;
+    var loginPage = open("http://localhost:9999", LoginPageV3.class);
+    var authInfo = DataHelper.getAuthInfo();
+    var verificationPage = loginPage.validLogin(authInfo);
+    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+    var dashboardPage = verificationPage.validVerify(verificationCode);
+    var firstCardInfo = DataHelper.getFirstCardBalance();
+    var secondCardInfo = DataHelper.getSecondCardBalance();
+    var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+    var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+    var amount = DataHelper.generateInvalidAmount(firstCardBalance);
+    var expectedBalanceFirstCard = firstCardBalance - amount;
+    var expectedBalanceSecondCard = secondCardBalance + amount;
+    var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+    transferPage.findErrorMessage("Ошибка! Сумма перевода не должна превышать баланс карты списания!");
+    dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+    var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+    var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+    assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+    assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+  }
+}
+
